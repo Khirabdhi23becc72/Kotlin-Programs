b160afa72dc18f9ab7f2eee874d9ee841dd28286
Khirabdhi23becc72 <ece.23becc72@silicon.ac.in>
2026-06-01 22:52:04 +0530

    q7

--- q7.kt
+++ q7.kt
@@ -1,2 +1,13 @@
-val user1 = User(1, "Jyoti", true)
-val user2 = user1.copy(isActive = false)
+data class User(
+    val id: Int,
+    val username: String,
+    val isActive: Boolean
+)
+
+fun main() {
+    val user1 = User(1, "Jyoti", true)
+    val user2 = user1.copy(isActive = false)
+
+    println(user1)
+    println(user2)
+}
